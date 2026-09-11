d1d8a48f676fac8c8969f73a0b2870f71fa18837
Marcio Kleber Junior Lopes de Souza <marciokjls@gmail.com>
2026-09-10 23:29:54 -0300

    First commit

---
+++ src/main/java/com/github/agentragpgvectorspring/AgentRagPgvectorSpringApplication.java
@@ -0,0 +1,13 @@
+package com.github.agentragpgvectorspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgentRagPgvectorSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgentRagPgvectorSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/agentragpgvectorspring/AgentRagPgvectorSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.agentragpgvectorspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgentRagPgvectorSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
